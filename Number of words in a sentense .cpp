7910376ed1fb80d0ba6e549217346f0b4192b75b
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-11-10 16:39:41 +0530

    Create Number of words in a sentense .cpp

---
+++ Number of words in a sentense .cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    int i=0;
+    int count;
+    cout << "enter an line \n";
+    getline(cin, s);
+   if(s[0]==' ')  
+   {
+       count=0;
+   }
+   else 
+   {
+       count=1;
+   }
+   
+   while(s[i]!='\0')
+   {
+     if(s[i]==' '&&s[i+1]!=' '&&s[i+1]!='\0')
+     {
+         count++;
+     }
+     i++;
+   }
+   cout<<"the number of words are "<<count<<endl;
+    return 0;
+}
