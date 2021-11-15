ba9b936285b388a605bef94e12d1ff1d7edae3e5
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-11-15 10:45:20 +0530

    Create Reverse a string.cpp

---
+++ Reverse a string.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    int l;
+    cout<<"enter an string "<<endl;
+    getline(cin,s);
+    l=s.length();
+    for(int i=l;i>=0;i--)
+    { 
+        cout<<s[i]<<" ";
+    }
+    return 0;
+}
